c9047bf9b81a640a024d7f7fd24035791fa7be10
Wei Zhao <874103271@qq.com>
2023-03-17 00:00:15 +0000

    create brunch

--- HelloGIt/src/main/java/com/atguigu/git/HelloGIt.java
+++ HelloGIt/src/main/java/com/atguigu/git/HelloGIt.java
@@ -4,5 +4,6 @@ public class HelloGIt {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("udpate1");
+        System.out.println("dev update");
     }
 }
